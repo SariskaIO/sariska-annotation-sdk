58dbd2f12ae4d154697605c2b9420e7ffecc346a
gurudeep88 <gurudeep88@gmail.com>
2024-02-11 01:40:47 +0530

    props modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import ScrollUpButton from "./components/ScrollUpButton";
-import MyComponent from "./components/MyComponent";
 import Canvas from "./components/Canvas";
 
 export {
-    ScrollUpButton,
-    MyComponent,
     Canvas
 }
